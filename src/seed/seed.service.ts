9e390922b4a26231ad330b25b7ffcb6a90cbadb1
Sergio Miranda <152328184+TitanDevelopMan@users.noreply.github.com>
2024-08-26 10:10:08 -0400

    antes de tarea

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -8,14 +8,23 @@ export class SeedService {
   private readonly axios: AxiosInstance = axios;
   async executedSeed() {
     const { data } = await this.axios.get<PokeResponse>(
-      'https://pokeapi.co/api/v2/pokemon?limit=650',
+      'https://pokeapi.co/api/v2/pokemon?limit=10',
     );
 
-    data.results.forEach(({name, url}) => {
-      console.log({name, url});
-        const segment = url.split('/');
-        
-    })
+    console.log(data);
+
+    // recorro los resultados
+    data.results.forEach(({ name, url }) => {
+      console.log({ name, url });
+      // tomo la url y la divido en segmentos
+      const segment = url.split('/');
+
+      // console.log(segment)
+      // tomo el penultimo segment
+      const no: number = +segment[segment.length - 2];
+      console.log({ name, no });
+      // asi obtengo un {name: Pokemon, no: numero}
+    });
     return data.results;
   }
 }
